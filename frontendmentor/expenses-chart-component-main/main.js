b73149a6841e84f247c032f399eb9f59bdc88d7e
That-1-JS_Nerd <itsNathan022@gmail.com>
2022-06-24 16:35:46 +0200

    all files

---
+++ frontendmentor/expenses-chart-component-main/main.js
@@ -0,0 +1,90 @@
+'use-strict';
+
+const charData = [
+  {
+    "day": "mon",
+    "amount": 17.45
+  },
+  {
+    "day": "tue",
+    "amount": 34.91
+  },
+  {
+    "day": "wed",
+    "amount": 52.36
+  },
+  {
+    "day": "thu",
+    "amount": 31.07
+  },
+  {
+    "day": "fri",
+    "amount": 23.39
+  },
+  {
+    "day": "sat",
+    "amount": 43.28
+  },
+  {
+    "day": "sun",
+    "amount": 25.48
+  }
+];
+
+const graph = document.querySelector('.graph');
+
+function highest() {
+  let ht = 0;
+  for (let i = 0; i < charData.length; i++) {
+    if (charData[i].amount > ht) {
+      ht = charData[i].amount;
+    }
+  }
+  return ht;
+}
+
+function createGraph() {
+  const hVal = highest();
+  const hv = 4;
+
+  for (let j = 0; j < charData.length; j++) {
+    graph.innerHTML += `
+    <div class="bar-sect">
+        <div class="tooltip">$${charData[j].amount}</div>
+        <div class="bar"></div>
+        <p>${charData[j].day}</p>
+    </div>
+    `;
+  }
+
+  const bars = document.querySelectorAll('.bar');
+  const tooltips = document.querySelectorAll('.tooltip');
+
+  bars.forEach(
+    (bar, idx) => {
+      bar.style.height = `${charData[idx].amount * 4}px`;
+      if (tooltips[idx].textContent === `$${hVal}`) {
+        bar.style.backgroundColor = `hsl(186, 34%, 60%)`;
+      }
+  });
+}
+
+function main () {
+    createGraph();
+
+    document.querySelectorAll('.bar-sect').forEach(
+        (bar) => {
+          const tool = bar.firstElementChild;
+          const graphBar = tool.nextElementSibling;
+
+          graphBar.addEventListener('mouseover', function() {
+            tool.style.display = 'block';
+          });
+
+          graphBar.addEventListener('mouseleave', function() {
+            tool.style.display = 'none';
+          });
+    });
+}
+
+main();
